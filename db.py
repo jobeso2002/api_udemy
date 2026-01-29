2bb2c4985c46c4cdf70afc9d9173322e9cfdd324
José Alfredo Obeso Lora <jobeso@unicesar.edu.co>
2026-01-29 11:54:17 -0600

    se hizo la conexion a la base de datos

---
+++ db.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URI = 'postgresql://jose:12345@localhost:5432/agenda'
+
+def conectar():
+    engine = create_engine(DATABASE_URI)
+    Session = sessionmaker(bind=engine)
+    s = Session()
+
+    if s != None:
+        print("Conexion a base de datos ok")
+    else:
+        print("Error en conexion a base de datos")
+
+    return s
+    
+
+
+
+
+
+
+
+
+
+    
+
+
--- routes/userRoutes.py
+++ routes/userRoutes.py
@@ -1,9 +1,22 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, session
 from flask import Blueprint
+from models import Usuario
 
+
+from db import conectar
 
 user_api = Blueprint('user_api', __name__)
 
 @user_api.route('/usuario', methods=['Get'])
 def usuario():
-    return "funcionando Usuario"
+    try:
+        session = conectar()
+        usuarios = session.query(Usuario).all()
+        print(usuarios)
+    except Exception as e:
+        print(e)
+    finally:
+        session.close()
+    
+    return "consulta correcta usuario"        
+
